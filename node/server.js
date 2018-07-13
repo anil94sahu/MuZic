25c85a0b0cc647ab4b94188376cd527047917af6
Anil Sahu <anil94sahu@gmail.com>
2018-07-13 09:18:43 +0530

    post api for songs --- unit testing remaining

--- node/server.js
+++ node/server.js
@@ -1,30 +1,38 @@
-const express = require('express');
-const bodyParser= require('body-parser')
-const app = express();
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
+var mongoose = require('mongoose');
+var product = require('./song');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var port = process.env.PORT || 8000;
+var router = express.Router();
 
-/* app.listen(8000, function() {
-   
-}) */
-var MongoClient = require('mongodb').MongoClient;
-var db;
-MongoClient.connect("mongodb://localhost:27017", function(err, client) {
-    if(err) throw err;
-    var db = client.db('MuZic');
-   
-  
-    // Start the application after the database connection is ready
-    app.listen(3000);
-    db.collection("albums").find({}, function(err, docs) 
-    { var resultArray  = [];
-      //  console.log(docs);
-        docs.forEach(function(doc) {
-            resultArray.push(doc);
-          }, function (err) {
-            console.log('results:', resultArray);
-            
-          });
-    })
-  });
+app.use(cors());
+app.use('/api', router);
+app.listen(port);
+console.log('REST API is runnning at ' + port);
 
+router.use(function (req, res, next) {
+  // do logging 
+  // do authentication 
+  console.log('Logging of request will be done here');
+  next(); // make sure we go to the next routes and don't stop here
+});
 
+router.route('/products').post(function (req, res) {
+  var p = new song();
+  p.id = req.body.id;
+  p.album_id = req.body.album_id;
+  p.artist_id = req.body.artist_id;
+  p.name = req.body.name;
+  p.filename = req.body.filename;
+  p.save(function (err) {
+      if (err) {
+          res.send(err);
+      }
+      res.send({ message: 'songs Created !' })
+  })
+});
